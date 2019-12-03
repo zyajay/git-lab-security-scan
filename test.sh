ba1b6d2f8bdee07cbe0a4c804fc2262dc015aeb6
Olivier Gonzalez <ogonzalez@gitlab.com>
2019-12-03 11:26:35 -0500

    Remove integration tests
    This is redundant with downstream QA pipelines

--- test.sh
+++ test.sh
@@ -1,31 +1,10 @@
 #!/bin/sh
 
 # Make it TAP compliant, see http://testanything.org/tap-specification.html
-echo "1..4"
+echo "1..2"
 
 failed=0
 step=1
-
-got="test/fixtures/gl-dependency-scanning-report.json"
-expect="test/expect/gl-dependency-scanning-report.json"
-
-export DS_DEFAULT_ANALYZERS=${DS_DEFAULT_ANALYZERS:-"bundler-audit,retire.js,gemnasium"}
-export DS_ANALYZER_IMAGE_TAG=${DS_ANALYZER_IMAGE_TAG:-"2"}
-export DS_EXCLUDED_PATHS="ignored,*-excluded"
-
-# Project found, artifact generated (bind mount)
-desc="Generate expected artifact (bind mount, pull images)"
-rm -f $got
-CI_PROJECT_DIR="$PWD/test/fixtures" ./dependency-scanning
-
-if test $? -eq 0 && diff -u $expect $got; then
-  echo "ok $step - $desc"
-else
-  echo "not ok $step - $desc"
-  failed=$((failed+1))
-fi
-step=$((step+1))
-echo
 
 # Project not found
 desc="Exit with exit status 3 when no compatible analyzer can be found"
@@ -46,21 +25,6 @@ mkdir -p "$PWD/test/fixtures/empty"
 CI_PROJECT_DIR="$PWD/test/fixtures/empty" DS_PULL_ANALYZER_IMAGES=0 ./dependency-scanning
 
 if [ $? -eq 4 ]; then
-  echo "ok $step - $desc"
-else
-  echo "not ok $step - $desc"
-  failed=$((failed+1))
-fi
-step=$((step+1))
-echo
-
-# Project found, artifact generated (bind mount, no pull, no remote checks)
-expect="test/expect/gl-dependency-scanning-report.no-remote-checks.json"
-desc="Generate expected artifact w/o remote checks"
-rm -f $got
-CI_PROJECT_DIR="$PWD/test/fixtures" DS_DISABLE_REMOTE_CHECKS=1 DS_PULL_ANALYZER_IMAGES=0 ./dependency-scanning
-
-if test $? -eq 0 && diff -u $expect $got; then
   echo "ok $step - $desc"
 else
   echo "not ok $step - $desc"
